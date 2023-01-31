bb74bcb4f34ef26314dc66e8d3e7d2a772ea86c5
Michiel Glibert <michiel.glibert@gmail.com>
2023-01-31 22:13:48 +0000

    test

--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,27 @@
-export default function middleware(request) {
+export default function middleware(request: Request) {
   const { headers } = request;
-  const userAgent = headers["user-agent"] || headers["User-Agent"];
   const host =
     "https://test-avatar-and-logo-upload-wegroup-nv.campaigns.staging.louiseforbrokers.be/#/?lang=nl";
+  const userAgent = headers.get("user-agent");
 
-  console.debug("Start");
   if (userAgent && host && Boolean(host.match(/\.campaigns|conversations\./))) {
     const agents =
       ".*(googlebot|adsbot-google|Feedfetcher-Google|bingbot|yandex|baiduspider|Facebot|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora link preview|showyoubot|outbrain|pinterest|slackbot|vkShare|W3C_Validator|redditbot|applebot|whatsapp|flipboard|tumblr|bitlybot|skypeuripreview|nuzzel|discordbot|google page speed|qwantify|pinterestbot|bitrix link preview|xing-contenttabreceiver|chrome-lighthouse|telegrambot).*";
     const prerender = new RegExp(agents, "i").test(userAgent);
-    console.debug("Host and agent were found");
     const url = new URL(
       `/?url=${host}`,
       "https://prerender-campaign.vercel.app"
     );
-    return Response.redirect(url);
-    if (prerender) {
-      const url = new URL(
-        `/?url=${host}`,
-        "https://prerender-campaign.vercel.app"
-      );
-      console.debug("Prerender should work");
-      console.debug(url.href);
-      return Response.redirect(url);
-    }
+
+    const prerenderRequest = new Request(url, request);
+    prerenderRequest.headers.set(
+      "origin",
+      "https://prerender-campaign.vercel.app"
+    );
+    prerenderRequest.headers.set(
+      "host",
+      "https://prerender-campaign.vercel.app"
+    );
+    return request;
   }
 }
